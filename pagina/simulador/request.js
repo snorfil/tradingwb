1495cbeb423f00cb39f20f431b1f90b6dd58fa1f
Edwin leandro <Snorfil@hotmail.com>
2022-05-04 21:32:51 +0200

    separacion de los pares recibidos

--- pagina/simulador/request.js
+++ pagina/simulador/request.js
@@ -1,30 +1,66 @@
 var lista = [];
 var nombrePares = [];
+var bnbData = [];
+var btcData = [];
+var ethData = [];
+var usdtData = [];
 async function pares(){
     var req = new XMLHttpRequest();
+    var cadena;
+    var id;
     
     await req.open('GET', 'https://api.binance.com/api/v3/ticker/price', false);
+
     req.send(null);
     if (req.status == 200)
         dump(req.responseText);
         let aux = JSON.parse(req.responseText);
+
+    
     
         for (let i = 0; i < aux.length; i++) {
-            nombrePares.push(aux[i]['symbol'])  
+
+            cadena = aux[i]['symbol'];
+
+            console.log(cadena);
+            if(/^BNB/.test(cadena)){
+                bnbData.push(cadena);
+                id = "bnb_selector";
+              
+            }
+            else if(/^BTC/.test(cadena))
+            {
+                btcData.push(cadena);
+                id = "btc_selector";
+            }
+            else if(/^USDT/.test(cadena))
+            {
+                usdtData.push(cadena);
+                id = "usdt_selector";
+            }
+            else if(/^ETH/.test(cadena))
+            {
+                ethData.push(cadena);
+                id = "eth_selector";
+            }
+
+            setOptionOn(id,cadena)
+                
+           
         }
-
-    //Create and append select list
-    var selectList = document.getElementById("selector");
+    
    
-    // asignamos opciones al componente selector
-    for (var i = 0; i < nombrePares.length; i++) {
-        var option = document.createElement("option");
-        option.value = nombrePares[i];
-        option.text = nombrePares[i];
-        selectList.appendChild(option);
-    }
-
 }
+function setOptionOn(id,valor)
+{
+    let selectList = document.getElementById(id);
+    let option = document.createElement("option");
+    option.value = valor;
+    option.text = valor;
+    selectList.appendChild(option);
+}
+
+
 
 async function getDataOf(par){
     var req = new XMLHttpRequest();
